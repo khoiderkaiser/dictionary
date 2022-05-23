61f30f710950520aea54ff09d0fce9f8a1dfa558
Gunter <18520943@ms.uit.edu.vn>
2022-05-23 12:37:57 +0700

    nada

--- app/src/main/java/com/example/dictionarydmkvtq/Adapters/PhoneticsAdapter.java
+++ app/src/main/java/com/example/dictionarydmkvtq/Adapters/PhoneticsAdapter.java
@@ -42,7 +42,7 @@ public class PhoneticsAdapter extends RecyclerView.Adapter<PhoneticViewHolder> {
                 MediaPlayer player = new MediaPlayer();
                 try{
                     player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    player.setDataSource("https"+phoneticsList.get(position).getAudio());
+                    player.setDataSource("https"+phoneticsList.get(holder.getAdapterPosition()).getAudio());
                     player.prepare();
                     player.start();
                 } catch (Exception e){
